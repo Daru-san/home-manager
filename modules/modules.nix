badd26e49dbd71b4650aa4ad9061f061066c677d
Daru <mica.tui@proton.me>
2024-11-18 08:17:00 +0200

    modules: import zen

--- modules/modules.nix
+++ modules/modules.nix
@@ -267,6 +267,7 @@ let
     ./programs/zathura.nix
     ./programs/zed-editor.nix
     ./programs/zellij.nix
+    ./programs/zen.nix
     ./programs/zk.nix
     ./programs/zoxide.nix
     ./programs/zplug.nix
